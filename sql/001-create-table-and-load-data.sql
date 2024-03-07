bd8944245e014b4d9268cc0852d18fb791687b96
”溝口光一” <guangyigoukou7@gmail.com>
2024-03-07 11:37:19 +0900

    tableをstudentsに一つにまとめてschoolyearのカラムを追加

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,33 +1,19 @@
-DROP TABLE IF EXISTS students_first_grade;
-DROP TABLE IF EXISTS students_second_grade;
-DROP TABLE IF EXISTS students_third_grade;
+DROP TABLE IF EXISTS students;
 
-CREATE TABLE students_first_grade (
+CREATE TABLE students (
  id int unsigned AUTO_INCREMENT,
  name VARCHAR(20) NOT NULL,
+ schoolyear VARCHAR(20) NOT NULL,
  birthplace VARCHAR(20) NOT NULL,
  PRIMARY KEY(id)
 );
 
-INSERT INTO students_first_grade (name,birthplace) VALUES ('溝口光一',"大分県");
-INSERT INTO students_first_grade (name,birthplace) VALUES ("海崎健斗","福岡県");
+INSERT INTO students (name,schoolyear,birthplace) VALUES ('溝口光一',"1年生","大分県");
+INSERT INTO students (name,schoolyear,birthplace) VALUES ("海崎健斗","1年生","福岡県");
 
-CREATE TABLE students_second_grade (
- id int unsigned AUTO_INCREMENT,
- name VARCHAR(20) NOT NULL,
- birthplace VARCHAR(20) NOT NULL,
- PRIMARY KEY(id)
-);
+INSERT INTO students (name,schoolyear,birthplace) VALUES ('溝口健太',"2年生","福岡県");
+INSERT INTO students (name,schoolyear,birthplace) VALUES ("海野芽衣","2年生","熊本県");
 
-INSERT INTO students_second_grade (name,birthplace) VALUES ('溝口健太',"福岡県");
-INSERT INTO students_second_grade (name,birthplace) VALUES ("海野芽衣","熊本県");
+INSERT INTO students (name,schoolyear,birthplace) VALUES ('溝口千尋',"3年生","熊本県");
+INSERT INTO students (name,schoolyear,birthplace) VALUES ("春夏冬徹","3年生","大分県");
 
-CREATE TABLE students_third_grade (
- id int unsigned AUTO_INCREMENT,
- name VARCHAR(20) NOT NULL,
- birthplace VARCHAR(20) NOT NULL,
- PRIMARY KEY(id)
-);
-
-INSERT INTO students_third_grade (name,birthplace) VALUES ('溝口千尋',"熊本県");
-INSERT INTO students_third_grade (name,birthplace) VALUES ("春夏冬徹","大分県")；
