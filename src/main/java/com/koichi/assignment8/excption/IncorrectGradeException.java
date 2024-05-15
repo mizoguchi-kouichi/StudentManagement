7eb6df2822f730fbfce203dd40f5e370fa3c27ad
”溝口光一” <guangyigoukou7@gmail.com>
2024-05-15 23:50:56 +0900

    javadocの追加

--- src/main/java/com/koichi/assignment8/excption/IncorrectGradeException.java
+++ src/main/java/com/koichi/assignment8/excption/IncorrectGradeException.java
@@ -1,5 +1,10 @@
 package com.koichi.assignment8.excption;
 
+/**
+ * updateGradeで進級する学年が指定されなかった時の
+ * 例外処理になります。
+ */
+
 public class IncorrectGradeException extends RuntimeException {
     public IncorrectGradeException(String message) {
         super(message);
--- src/main/java/com/koichi/assignment8/excption/StudentNotFoundException.java
+++ src/main/java/com/koichi/assignment8/excption/StudentNotFoundException.java
@@ -1,5 +1,9 @@
 package com.koichi.assignment8.excption;
 
+/**
+ * GETリクエストで指定したidのstudentがいなかった際の
+ * 例外処理になります。
+ */
 public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
         super(message);
