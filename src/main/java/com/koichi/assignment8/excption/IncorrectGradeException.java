4a3a8d9c8d3e756a35501fc2682147e155641280
”溝口光一” <guangyigoukou7@gmail.com>
2024-05-17 13:03:16 +0900

    空行の削除

--- src/main/java/com/koichi/assignment8/excption/IncorrectGradeException.java
+++ src/main/java/com/koichi/assignment8/excption/IncorrectGradeException.java
@@ -4,7 +4,6 @@ package com.koichi.assignment8.excption;
  * updateGradeで進級する学年が指定されなかった時の
  * 例外処理になります。
  */
-
 public class IncorrectGradeException extends RuntimeException {
     public IncorrectGradeException(String message) {
         super(message);
--- src/main/java/com/koichi/assignment8/excption/MultipleMethodsException.java
+++ src/main/java/com/koichi/assignment8/excption/MultipleMethodsException.java
@@ -4,7 +4,6 @@ package com.koichi.assignment8.excption;
  * GETリクエストのクエリパラメータ検索でカラムを2つ以上を選んだ時の
  * 例外処理になります。
  */
-
 public class MultipleMethodsException extends RuntimeException {
 
     public MultipleMethodsException(String message) {
