3fc023fe86168de593870d5ba7e621f467c4fba9
”溝口光一” <guangyigoukou7@gmail.com>
2024-05-27 22:54:42 +0900

    UpdateFailedExceptionの削除

--- src/main/java/com/koichi/assignment8/excption/StudentControllerAdvice.java
+++ src/main/java/com/koichi/assignment8/excption/StudentControllerAdvice.java
@@ -40,17 +40,6 @@ public class StudentControllerAdvice {
         return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = UpdateFailedException.class)
-    public ResponseEntity<Map<String, String>> NoStudentArePromotedException(
-            UpdateFailedException e, HttpServletRequest request) {
-        Map<String, String> body = Map.of(
-                "timestamp", ZonedDateTime.now().toString(),
-                "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
-                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                "message", e.getMessage(),
-                "path", request.getRequestURI());
-        return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> MethodArgumentNotValidException(MethodArgumentNotValidException e) {
