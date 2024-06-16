f93eada9c87dba0ec027f5813fba4498bcbbecab
”溝口光一” <guangyigoukou7@gmail.com>
2024-06-16 17:27:22 +0900

    ワイルドカードインポートの削除

--- src/test/java/com/koichi/assignment8/service/StudentServiceTest.java
+++ src/test/java/com/koichi/assignment8/service/StudentServiceTest.java
@@ -15,7 +15,9 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
