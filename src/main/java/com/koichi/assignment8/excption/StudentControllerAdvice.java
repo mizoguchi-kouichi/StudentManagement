78f05664e1d55614a547db2cfe53d47d5bab3d22
”溝口光一” <guangyigoukou7@gmail.com>
2024-07-24 09:09:47 +0900

    学生のデータを更新する際にリクエストされたIDが空白の場合handleMissingPathVariableExceptionのレスポンスボティが返却される結合テストの作成

--- src/main/java/com/koichi/assignment8/excption/StudentControllerAdvice.java
+++ src/main/java/com/koichi/assignment8/excption/StudentControllerAdvice.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -44,7 +45,6 @@ public class StudentControllerAdvice {
         return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
     }
 
-
     /**
      * 以下の二つについての例外処理です。
      * 読み取り処理・登録処理・削除処理のID検索の際に文字列がリクエストされた場合
@@ -58,6 +58,22 @@ public class StudentControllerAdvice {
                 "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
                 "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 "message", "IDまたは学年を入力する際は、半角の数字で入力してください",
+                "path", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 更新処理・削除処理のID検索の際空白がリクエストされた場合の
+     * 例外処理です。
+     */
+    @ExceptionHandler(value = MissingPathVariableException.class)
+    public ResponseEntity<Map<String, String>> handleMissingPathVariableException(
+            MissingPathVariableException ex, HttpServletRequest request) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().format(formatter),
+                "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "message", "学生のIDを入力してください",
                 "path", request.getRequestURI());
         return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
     }
--- src/test/java/com/koichi/assignment8/itegrationtest/studentApiIntegrationTest.java
+++ src/test/java/com/koichi/assignment8/itegrationtest/studentApiIntegrationTest.java
@@ -609,4 +609,37 @@ public class studentApiIntegrationTest {
                             """));
         }
     }
+
+    @Test
+    @DataSet(value = "datasets/students.yml")
+    @ExpectedDataSet(value = "datasets/students.yml")
+    @Transactional
+    void 学生のデータを更新する際にリクエストされたIDが空白の場合handleMissingPathVariableExceptionのレスポンスボティが返却されること() throws Exception {
+
+        final ZonedDateTime fixedClock = ZonedDateTime.of(2024, 1, 1, 0, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+
+        try (MockedStatic<ZonedDateTime> mockClock = Mockito.mockStatic(ZonedDateTime.class)) {
+            mockClock.when(ZonedDateTime::now).thenReturn(fixedClock);
+
+            mockMvc.perform(MockMvcRequestBuilders.patch("/students/ ")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("""
+                                    {
+                                        "name":"城野健一",
+                                        "grade":"二年生",
+                                        "birthPlace":"福岡県"
+                                    }
+                                    """))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andExpect(MockMvcResultMatchers.content().json("""
+                            {
+                                "timestamp": "2024/01/01 T00:00:00+0900［Asia/Tokyo］",
+                                "error": "Bad Request",
+                                "path": "/students/%20",
+                                "status": "400",
+                                "message": "学生のIDを入力してください"
+                            }
+                            """));
+        }
+    }
 }
