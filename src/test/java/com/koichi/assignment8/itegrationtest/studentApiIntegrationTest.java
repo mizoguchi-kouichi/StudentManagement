cd022441ef7d75a0d21d8ca066c5018678946ebe
”溝口光一” <guangyigoukou7@gmail.com>
2024-07-24 09:14:21 +0900

    学生のデータを更新する際に学年がない時にValidationErrorのレスポンスボティを返す結合テストの作成

--- src/test/java/com/koichi/assignment8/itegrationtest/studentApiIntegrationTest.java
+++ src/test/java/com/koichi/assignment8/itegrationtest/studentApiIntegrationTest.java
@@ -712,4 +712,41 @@ public class studentApiIntegrationTest {
                             """));
         }
     }
+
+    @Test
+    @DataSet(value = "datasets/students.yml")
+    @ExpectedDataSet(value = "datasets/students.yml")
+    @Transactional
+    void 学生のデータを更新する際に学年がない時にValidationErrorのレスポンスボティを返すこと() throws Exception {
+
+        final ZonedDateTime fixedClock = ZonedDateTime.of(2024, 1, 1, 0, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+
+        try (MockedStatic<ZonedDateTime> mockClock = Mockito.mockStatic(ZonedDateTime.class)) {
+            mockClock.when(ZonedDateTime::now).thenReturn(fixedClock);
+
+            mockMvc.perform(MockMvcRequestBuilders.patch("/students/1")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("""
+                                    {
+                                        "name":"城野健一",
+                                        "grade":"",
+                                        "birthPlace":"福岡県"
+                                    }
+                                    """))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andExpect(MockMvcResultMatchers.content().json("""
+                            {
+                                "status": "400",
+                                "message": "validation error",
+                                "timestamp": "2024/01/01 T00:00:00+0900［Asia/Tokyo］",
+                                "errors": [
+                                    {
+                                        "field": "grade",
+                                        "message": "有効な学年を指定してください（一年生, 二年生, 三年生,卒業生のいずれか）。"
+                                    }
+                                ]
+                            }
+                            """));
+        }
+    }
 }
