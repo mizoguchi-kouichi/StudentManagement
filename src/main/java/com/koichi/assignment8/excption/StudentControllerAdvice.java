7c6638a39668778149e5de7ba0d97ce5df7e6fa5
”溝口光一” <guangyigoukou7@gmail.com>
2024-07-24 09:11:56 +0900

    学生のデータを更新する際に全学生をリクエストされたの場合handleHttpRequestMethodNotSupportedExceptionのレスポンスボティが返却される結合テストの作成

--- src/main/java/com/koichi/assignment8/excption/StudentControllerAdvice.java
+++ src/main/java/com/koichi/assignment8/excption/StudentControllerAdvice.java
@@ -3,6 +3,7 @@ package com.koichi.assignment8.excption;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -69,6 +70,22 @@ public class StudentControllerAdvice {
     @ExceptionHandler(value = MissingPathVariableException.class)
     public ResponseEntity<Map<String, String>> handleMissingPathVariableException(
             MissingPathVariableException ex, HttpServletRequest request) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().format(formatter),
+                "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "message", "学生のIDを入力してください",
+                "path", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 更新処理・削除処理の際に全学生がリクエストされた場合の
+     * 例外処理です。
+     */
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Map<String, String>> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException ex, HttpServletRequest request) {
         Map<String, String> body = Map.of(
                 "timestamp", ZonedDateTime.now().format(formatter),
                 "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
--- src/test/java/com/koichi/assignment8/itegrationtest/studentApiIntegrationTest.java
+++ src/test/java/com/koichi/assignment8/itegrationtest/studentApiIntegrationTest.java
@@ -642,4 +642,38 @@ public class studentApiIntegrationTest {
                             """));
         }
     }
+
+    @Test
+    @DataSet(value = "datasets/students.yml")
+    @ExpectedDataSet(value = "datasets/students.yml")
+    @Transactional
+    void 学生のデータを更新する際に全学生をリクエストされたの場合handleHttpRequestMethodNotSupportedExceptionのレスポンスボティが返却されること() throws Exception {
+
+        final ZonedDateTime fixedClock = ZonedDateTime.of(2024, 1, 1, 0, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+
+        try (MockedStatic<ZonedDateTime> mockClock = Mockito.mockStatic(ZonedDateTime.class)) {
+            mockClock.when(ZonedDateTime::now).thenReturn(fixedClock);
+
+            mockMvc.perform(MockMvcRequestBuilders.patch("/students")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("""
+                                    {
+                                        "name":"城野健一",
+                                        "grade":"二年生",
+                                        "birthPlace":"福岡県"
+                                    }
+                                    """))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andExpect(MockMvcResultMatchers.content().json("""
+                            {
+                                "status": "400",
+                                "message": "学生のIDを入力してください",
+                                "timestamp": "2024/01/01 T00:00:00+0900［Asia/Tokyo］",
+                                "error": "Bad Request",
+                                "path": "/students"
+                            }
+                            """));
+        }
+    }
+
 }
