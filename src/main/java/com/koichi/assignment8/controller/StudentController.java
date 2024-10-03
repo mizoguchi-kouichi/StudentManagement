098035e0a2022db1397da2ea883e1195f13c62ba
”溝口光一” <guangyigoukou7@gmail.com>
2024-10-03 19:05:15 +0900

    各エンドポイントの＠ApiResponsesの修正

--- src/main/java/com/koichi/assignment8/controller/StudentController.java
+++ src/main/java/com/koichi/assignment8/controller/StudentController.java
@@ -9,13 +9,13 @@ import com.koichi.assignment8.excption.StudentControllerAdvice;
 import com.koichi.assignment8.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -30,36 +30,12 @@ import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 
-@ApiResponses(
-        {
-                @ApiResponse(
-                        responseCode = "200",
-                        description = "ok",
-                        content = @Content(
-                                mediaType = "application/json",
-                                schema = @Schema(implementation = Student.class))),
-
-                @ApiResponse(
-                        responseCode = "400",
-                        description = "Bat Request",
-                        content = @Content(
-                                mediaType = "application/json",
-                                schema = @Schema(implementation = StudentControllerAdvice.ValidationErrorResponse.class))),
-
-                @ApiResponse(
-                        responseCode = "404",
-                        description = "student not found",
-                        content = @Content(
-                                mediaType = "application/json",
-                                schema = @Schema(implementation = StudentControllerAdvice.ValidationErrorResponse.class))),
-                @ApiResponse(
-                        responseCode = "500",
-                        description = "Internal server error",
-                        content = @Content(
-                                mediaType = "application/json",
-                                schema = @Schema(implementation = ErrorResponse.class)))
-        }
-)
+@ApiResponse(
+        responseCode = "500",
+        description = "Internal server error",
+        content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = StudentControllerAdvice.ErrorResponse.class)))
 @RestController
 public class StudentController {
     private final StudentService studentService;
@@ -74,6 +50,28 @@ public class StudentController {
      */
     @Operation(summary = "生徒取得API",
             description = "指定したidのstudentのデータを全て取得します"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ok",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Student.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentControllerAdvice.ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "student not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentControllerAdvice.ErrorResponse.class))),
+            }
     )
     @GetMapping("/students/{id}")
     public Student findById(@PathVariable("id") String id) {
@@ -92,6 +90,22 @@ public class StudentController {
      */
     @Operation(summary = "詳細検索API",
             description = "このエンドポイントでは全学生の情報を取得できます。また、特定のカラムを指定して学生を検索することも可能です。ただし、検索に使用できるカラムは一度に1つのみです。"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ok",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Student.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentControllerAdvice.ErrorResponse.class)))
+            }
     )
     @GetMapping("/students")
     public List<Student> getStudents(@RequestParam(required = false) String grade, @RequestParam(required = false) String startsWith, @RequestParam(required = false) String birthPlace) {
@@ -112,6 +126,37 @@ public class StudentController {
     @Operation(summary = "学生追加API",
             description = "名前、学年、出身地を入力してリクエストすると、新しい学生を追加できます。"
     )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ok",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentResponse.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(oneOf = {
+                                            StudentControllerAdvice.ErrorResponse.class,
+                                            StudentControllerAdvice.ValidationErrorResponse.class
+                                    }),
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "ErrorResponse",
+                                                    value = "{\"timestamp\":\"string\",\"status\":\"string\",\"error\":\"string\",\"message\":\"string\",\"path\":\"string\"}"
+                                            ),
+                                            @ExampleObject(
+                                                    name = "ValidationErrorResponse",
+                                                    value = "{\"status\":\"string\",\"message\":\"string\",\"timestamp\":\"string\",\"errors\":[{\"additionalProp1\":\"string\",\"additionalProp2\":\"string\",\"additionalProp3\":\"string\"}]}"
+                                            )
+                                    }
+                            )
+                    )
+            }
+    )
     @PostMapping("/students")
     public ResponseEntity<StudentResponse> insertStudent(@RequestBody @Validated StudentPostRequest studentPostRequest, UriComponentsBuilder uriBuilder) {
         Student student = studentService.insertStudent(studentPostRequest.getName(), studentPostRequest.getGrade(), studentPostRequest.getBirthPlace());
@@ -126,6 +171,43 @@ public class StudentController {
      */
     @Operation(summary = "学生更新API",
             description = "指定したidの生徒の情報を更新できます。"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ok",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentResponse.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(oneOf = {
+                                            StudentControllerAdvice.ErrorResponse.class,
+                                            StudentControllerAdvice.ValidationErrorResponse.class
+                                    }),
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "ErrorResponse",
+                                                    value = "{\"timestamp\":\"string\",\"status\":\"string\",\"error\":\"string\",\"message\":\"string\",\"path\":\"string\"}"
+                                            ),
+                                            @ExampleObject(
+                                                    name = "ValidationErrorResponse",
+                                                    value = "{\"status\":\"string\",\"message\":\"string\",\"timestamp\":\"string\",\"errors\":[{\"additionalProp1\":\"string\",\"additionalProp2\":\"string\",\"additionalProp3\":\"string\"}]}"
+                                            )
+                                    }
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "student not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentControllerAdvice.ErrorResponse.class)))
+            }
     )
     @PatchMapping("/students/{id}")
     public ResponseEntity<StudentResponse> updateStudent(@PathVariable("id") String id, @RequestBody @Validated StudentUpdateRequest studentUpdateRequest) {
@@ -147,6 +229,12 @@ public class StudentController {
     @Operation(summary = "全学年更新API",
             description = "全生徒の学年を更新できます。"
     )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Ok",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = StudentResponse.class)))
     @PatchMapping("/students/grade/_batchUpdate")
     public ResponseEntity<StudentResponse> updateGrade() {
         studentService.updateGrade();
@@ -160,6 +248,28 @@ public class StudentController {
      */
     @Operation(summary = "学生削除API",
             description = "指定したidの学生を削除出来ます。"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ok",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentResponse.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentControllerAdvice.ErrorResponse.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "student not found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = StudentControllerAdvice.ErrorResponse.class))),
+            }
     )
     @DeleteMapping("/students/{id}")
     public ResponseEntity<StudentResponse> deleteStudent(@PathVariable("id") String id) {
