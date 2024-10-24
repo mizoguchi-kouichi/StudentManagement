7fadfaf0c697d21487a03af12cc7fc236e0733bd
”溝口光一” <guangyigoukou7@gmail.com>
2024-10-24 11:59:12 +0900

    entityパッケージとexceptionパッケージの各クラスにJavadocを追加

--- src/main/java/com/koichi/assignment8/excption/MethodArgumentTypeMismatchException.java
+++ src/main/java/com/koichi/assignment8/excption/MethodArgumentTypeMismatchException.java
@@ -1,5 +1,8 @@
 package com.koichi.assignment8.excption;
 
+/**
+ * idを指定するCRUD処理やクエリパラメータ検索するREAD処理の際に、指定する数値が数字以外でリクエストされた時の例外処理です。
+ */
 public class MethodArgumentTypeMismatchException extends RuntimeException {
 
     public MethodArgumentTypeMismatchException(String message) {
--- src/main/java/com/koichi/assignment8/excption/StudentNotFoundException.java
+++ src/main/java/com/koichi/assignment8/excption/StudentNotFoundException.java
@@ -1,12 +1,10 @@
 package com.koichi.assignment8.excption;
 
 /**
- * GETリクエストで指定したidのstudentがいなかった際の
- * 例外処理になります。
+ * idを指定するCLOD処理で学生が見つからない場合の例外処理になります。
  */
 public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
         super(message);
     }
-
 }
